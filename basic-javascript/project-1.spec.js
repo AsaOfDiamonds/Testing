26f57c219fecfa6aab18ca50143f044520fbba7e
1@$@1github <asaofdiamonds@gmail.com>
2019-01-28 17:48:35 -0700

    first commit, with multiply by 10

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,27 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+describe('project-1 testing', () => {
+
+  describe('multiply by ten function', () => {
+    it('checks for a number or intended number input', () => {
+      expect(helpers.multiplyByTen('4')).toBe(40);
+      expect(helpers.multiplyByTen([1, 2, 3])).toBe(NaN);
+      expect(helpers.multiplyByTen('four')).toBe(NaN);
+    });
+
+    it('multiplies by ten', () => {
+      expect(helpers.multiplyByTen(10)).toEqual(100);
+    });
+
+    it('able to multiply negative numbers', () => {
+      expect(helpers.multiplyByTen(-10)).toBe(-100);
+    });
+  });
+
+
+
+
+  
+});
